4acf445b324ddea6a44b1c34824ecae7526272c1
Dariusz Okoński <darek200180@gmail.com>
2025-02-05 20:05:25 +0100

    add client

--- client/app.js
+++ client/app.js
@@ -1,18 +1,11 @@
-async function fetchGreeting() {
-  const response = await fetch('http://localhost:9000/', {
+function fetchGreeting() {
+  fetch('http://localhost:9000', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      query: 'query { greeting }',
+      query: '',
     }),
   });
-
-  const { data } = await response.json();
-  return data.greeting;
 }
-
-fetchGreeting().then((greeting) => {
-  document.getElementById('greeting').textContent = greeting;
-});
