92797660972e5190d22014762106dee80cf63804
Dariusz Okoński <darek200180@gmail.com>
2025-02-11 20:15:17 +0100

    map createdAt to date

--- server/resolvers.js
+++ server/resolvers.js
@@ -4,4 +4,13 @@ export const resolvers = {
   Query: {
     jobs: () => getJobs(),
   },
+
+  // casting createdAt to date as it is in Job schema
+  Job: {
+    date: (job) => toIsoDate(job.createdAt),
+  },
 };
+
+function toIsoDate(value) {
+  return value.slice(0, 'yyyy-mm-dd'.length);
+}
