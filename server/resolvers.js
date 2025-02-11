f63d0c308ecc2d8e6dca6f2ea39b1b80d64ebd67
Dariusz Okoński <darek200180@gmail.com>
2025-02-11 20:04:08 +0100

    getJobs from db

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,18 +1,7 @@
+import { getJobs } from './db/jobs.js';
+
 export const resolvers = {
   Query: {
-    jobs: () => {
-      return [
-        {
-          id: '111',
-          title: 'The title 1',
-          description: 'descriptions 1',
-        },
-        {
-          id: '222',
-          title: 'The title 2',
-          description: 'descriptions 2',
-        },
-      ];
-    },
+    jobs: () => getJobs(),
   },
 };
