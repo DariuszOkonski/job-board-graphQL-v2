cc5beb874f85977d025805b04224661f3c0a5fef
Dariusz Okoński <darek200180@gmail.com>
2025-03-11 18:08:12 +0100

    add update job

--- server/resolvers.js
+++ server/resolvers.js
@@ -4,6 +4,7 @@ import {
   getJob,
   getJobs,
   getJobsByCompany,
+  updateJob,
 } from './db/jobs.js';
 import { getCompany } from './db/companies.js';
 import { GraphQLError } from 'graphql';
@@ -38,6 +39,10 @@ export const resolvers = {
     },
 
     deleteJob: (_root, { id }) => deleteJob(id),
+
+    updateJob: (_root, { input: { id, title, description } }) => {
+      return updateJob({ id, title, description });
+    },
   },
 
   Company: {
